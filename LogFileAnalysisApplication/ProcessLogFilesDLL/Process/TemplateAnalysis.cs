a4695236f094cc8b42d829ddac80b7d8d05f2312
Vadim Bratskyi <vadymbratskyi@gmail.com>
2020-02-26 22:08:33 +0200

    parse file

--- LogFileAnalysisApplication/ProcessLogFilesDLL/Process/TemplateAnalysis.cs
+++ LogFileAnalysisApplication/ProcessLogFilesDLL/Process/TemplateAnalysis.cs
@@ -11,6 +11,7 @@ namespace ProcessLogFilesDLL.Process {
         public string RegStart => @"(=>.*)";
         public string RegEnd => @"(<=.*)";
         public string RegError => @"""error""";
+        public string RegIsEmpty => @"{""isEmpty"":true}";
         public string RegDate => @"([0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2})\s(1[0-2]|[0-9]):(00|0[1-9]{1}|[1-5]{1}[0-9]):(00|0[1-9]{1}|[1-5]{1}[0-9])\s(PM|AM)";
 
     }
