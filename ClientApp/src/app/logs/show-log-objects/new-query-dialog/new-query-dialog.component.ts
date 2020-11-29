5512345e1b3912c382baa0f67952e0dd8319e6db
Vadim Bratskyi <vadymbratskyi@gmail.com>
2020-11-29 20:52:42 +0200

    build query result settings

--- ClientApp/src/app/logs/show-log-objects/new-query-dialog/new-query-dialog.component.ts
+++ ClientApp/src/app/logs/show-log-objects/new-query-dialog/new-query-dialog.component.ts
@@ -8,237 +8,267 @@ import { ShowLogObjectsService } from '@log_services';
 import { BehaviorSubject, ReplaySubject } from 'rxjs';
 
 export class TodoItemNode {
-  children: TodoItemNode[];
-  item: string;
-  value: string;
-  jObjectType: JObjectType;
+	children: TodoItemNode[];
+	item: string;
+	value: string;
+	jObjectType: JObjectType;
+	parentName: string;
+	path: string;
 }
 
 export class TodoItemFlatNode {
-  item: string;
-  value: string;
-  level: number;
-  expandable: boolean;
-  objectType: JObjectType;
+	item: string;
+	value: string;
+	level: number;
+	expandable: boolean;
+	objectType: JObjectType;
+	parentName: string;
+	path: string;
+}
+
+
+export class QuerySettingsKey {
+	public key: string;
+	public level: number;
+}
+
+export class QuerySettingsItem {
+	public name: string
+	public keys: QuerySettingsKey[];
+
+	constructor() {
+		this.name = '';
+		this.keys = [];
+	}
 }
 
 @Injectable()
-export class ChecklistDatabase {
-  dataChange = new BehaviorSubject<TodoItemNode[]>([]);
+	export class ChecklistDatabase {
+	dataChange = new BehaviorSubject<TodoItemNode[]>([]);
 
-  constructor(
-    private showLogObjectsService: ShowLogObjectsService
-  ) {
-    this.initialize();
-  }
+	constructor(
+		private showLogObjectsService: ShowLogObjectsService
+	) {
+		this.initialize();
+	}
 
-  initialize() {
-    this.showLogObjectsService.getAccesQueryForConfig()
-      .subscribe((accesQueries: QueryBuilderConfig) => {
-        const data = this.buildTreeFields(accesQueries.fields);
-        this.dataChange.next(data);
-    });    
-  }
+	initialize() {
+		this.showLogObjectsService.getAccesQueryForConfig()
+		.subscribe((accesQueries: QueryBuilderConfig) => {
+			const data = this.buildTreeFields(accesQueries.fields);
+			this.dataChange.next(data);
+		});
+	}
 
-  buildTreeFields(logQuery: LogQuery[]): TodoItemNode[] {
-    return logQuery.reduce<TodoItemNode[]>((accumulator, jobject) => {
-        const node = new TodoItemNode();
-        switch(jobject.objectType) {
-          case JObjectType.jobject:
-            node.item = `${jobject.key} { }`;
-            node.jObjectType = JObjectType.jobject;
-            break;
-          case JObjectType.jarray:
-            node.item = `${jobject.key} [ ]`;
-            node.jObjectType = JObjectType.jarray;
-            break;
-          default:
-            node.item = jobject.key;
-            node.jObjectType = JObjectType.none;
-            break;
-        }
-        node.value = jobject.key;
-        if (jobject.childrens.length > 0) {
-          node.children = this.buildTreeFields(jobject.childrens);
-        }
-        return accumulator.concat(node);
-      }, []
-    );
-  }
+	buildTreeFields(logQuery: LogQuery[], parentName?: string, pathNode?: string): TodoItemNode[] {
+		return logQuery.reduce<TodoItemNode[]>((accumulator, jobject) => {
+			const node = new TodoItemNode();
+			switch(jobject.objectType) {
+				case JObjectType.jobject:
+					node.item = `${jobject.key} { }`;
+					node.jObjectType = JObjectType.jobject;
+					break;
+				case JObjectType.jarray:
+					node.item = `${jobject.key} [ ]`;
+					node.jObjectType = JObjectType.jarray;
+					break;
+				default:
+					node.item = jobject.key;
+					node.jObjectType = JObjectType.none;
+					break;
+			}
+			node.value = jobject.key;
+			node.parentName = parentName;
+			node.path = pathNode ? `${pathNode}.${jobject.key}` : jobject.key;
+			if (jobject.childrens.length > 0) {
+			node.children = this.buildTreeFields(jobject.childrens, jobject.key, node.path);
+		}
+			return accumulator.concat(node);
+		}, []);
+	}
 
 }
 
 @Component({
-  selector: 'app-new-query-dialog',
-  templateUrl: './new-query-dialog.component.html',
-  styleUrls: ['./new-query-dialog.component.scss'],
-  providers: [ChecklistDatabase]
+	selector: 'app-new-query-dialog',
+	templateUrl: './new-query-dialog.component.html',
+	styleUrls: ['./new-query-dialog.component.scss'],
+	providers: [ChecklistDatabase]
 })
-export class NewQueryDialogComponent implements OnInit {
+	export class NewQueryDialogComponent implements OnInit {
 
-  private destroyed$: ReplaySubject<boolean> = new ReplaySubject();
+	private destroyed$: ReplaySubject<boolean> = new ReplaySubject();
 
-  constructor(
-    private _database: ChecklistDatabase,
-    public dialogRef: MatDialogRef<NewQueryDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+	constructor(
+		private _database: ChecklistDatabase,
+		public dialogRef: MatDialogRef<NewQueryDialogComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: any) {
 
-      this.treeFlattener = new MatTreeFlattener(this.transformer, this.getLevel,
-        this.isExpandable, this.getChildren);
-      this.treeControl = new FlatTreeControl<TodoItemFlatNode>(this.getLevel, this.isExpandable);
-      this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+		this.treeFlattener = new MatTreeFlattener(this.transformer, this.getLevel,
+			this.isExpandable, this.getChildren);
+		this.treeControl = new FlatTreeControl<TodoItemFlatNode>(this.getLevel, this.isExpandable);
+		this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
-  }
+	}
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+	onNoClick(): void {
+		this.dialogRef.close();
+	}
 
-  ngOnInit(): void {
-    this._database.dataChange.subscribe(data => {
-      this.dataSource.data = data;
-    });
-  }
+	ngOnInit(): void {
+		this._database.dataChange.subscribe(data => {
+		this.dataSource.data = data;
+		});
+	}
 
-  ngOnDestroy() {
-    this.destroyed$.next(true);
-    this.destroyed$.complete();
-  }
+	ngOnDestroy() {
+		this.destroyed$.next(true);
+		this.destroyed$.complete();
+	}
 
 
-  /** Map from flat node to nested node. This helps us finding the nested node to be modified */
-  flatNodeMap = new Map<TodoItemFlatNode, TodoItemNode>();
+	/** Map from flat node to nested node. This helps us finding the nested node to be modified */
+	flatNodeMap = new Map<TodoItemFlatNode, TodoItemNode>();
 
-  /** Map from nested node to flattened node. This helps us to keep the same object for selection */
-  nestedNodeMap = new Map<TodoItemNode, TodoItemFlatNode>();
+	/** Map from nested node to flattened node. This helps us to keep the same object for selection */
+	nestedNodeMap = new Map<TodoItemNode, TodoItemFlatNode>();
 
-  /** A selected parent node to be inserted */
-  // selectedParent: TodoItemFlatNode | null = null;
+	/** A selected parent node to be inserted */
+	// selectedParent: TodoItemFlatNode | null = null;
 
-  /** The new item's name */
-  // newItemName = '';
+	/** The new item's name */
+	// newItemName = '';
 
-  treeControl: FlatTreeControl<TodoItemFlatNode>;
+	treeControl: FlatTreeControl<TodoItemFlatNode>;
 
-  treeFlattener: MatTreeFlattener<TodoItemNode, TodoItemFlatNode>;
+	treeFlattener: MatTreeFlattener<TodoItemNode, TodoItemFlatNode>;
 
-  dataSource: MatTreeFlatDataSource<TodoItemNode, TodoItemFlatNode>;
+	dataSource: MatTreeFlatDataSource<TodoItemNode, TodoItemFlatNode>;
 
-  /** The selection for checklist */
-  checklistSelection = new SelectionModel<TodoItemFlatNode>(true /* multiple */);
+	/** The selection for checklist */
+	checklistSelection = new SelectionModel<TodoItemFlatNode>(true /* multiple */);
 
-  getLevel = (node: TodoItemFlatNode) => node.level;
+	queriesToSettins: QuerySettingsItem[] = [];
 
-  isExpandable = (node: TodoItemFlatNode) => node.expandable;
+	getLevel = (node: TodoItemFlatNode) => node.level;
 
-  getChildren = (node: TodoItemNode): TodoItemNode[] => node.children;
+	isExpandable = (node: TodoItemFlatNode) => node.expandable;
 
-  hasChild = (_: number, _nodeData: TodoItemFlatNode) => _nodeData.expandable;
+	getChildren = (node: TodoItemNode): TodoItemNode[] => node.children;
 
-  // hasNoContent = (_: number, _nodeData: TodoItemFlatNode) => _nodeData.item === '';
+	hasChild = (_: number, _nodeData: TodoItemFlatNode) => _nodeData.expandable;
 
-  /**
-   * Transformer to convert nested node to flat node. Record the nodes in maps for later use.
-   */
-  transformer = (node: TodoItemNode, level: number) => {
-    const existingNode = this.nestedNodeMap.get(node);
-    const flatNode = existingNode && existingNode.item === node.item
-        ? existingNode
-        : new TodoItemFlatNode();
-    flatNode.item = node.item;
-    flatNode.level = level;
-    flatNode.value = node.value;
-    flatNode.objectType = node.jObjectType;
-    flatNode.expandable = !!node.children?.length;
-    this.flatNodeMap.set(flatNode, node);
-    this.nestedNodeMap.set(node, flatNode);
-    return flatNode;
-  }
+	// hasNoContent = (_: number, _nodeData: TodoItemFlatNode) => _nodeData.item === '';
 
-  /** Whether all the descendants of the node are selected. */
-  descendantsAllSelected(node: TodoItemFlatNode): boolean {
-    const descendants = this.treeControl.getDescendants(node);
-    const descAllSelected = descendants.length > 0 && descendants.every(child => {
-      return this.checklistSelection.isSelected(child);
-    });
-    return descAllSelected;
-  }
+	/**
+		* Transformer to convert nested node to flat node. Record the nodes in maps for later use.
+		*/
+	transformer = (node: TodoItemNode, level: number) => {
+		const existingNode = this.nestedNodeMap.get(node);
+		const flatNode = existingNode && existingNode.item === node.item
+			? existingNode
+			: new TodoItemFlatNode();
+		flatNode.item = node.item;
+		flatNode.level = level;
+		flatNode.value = node.value;
+		flatNode.objectType = node.jObjectType;
+		flatNode.parentName = node.parentName;
+		flatNode.path = node.path;
+		flatNode.expandable = !!node.children?.length;
+		this.flatNodeMap.set(flatNode, node);
+		this.nestedNodeMap.set(node, flatNode);
+		return flatNode;
+	}
 
-  /** Whether part of the descendants are selected */
-  descendantsPartiallySelected(node: TodoItemFlatNode): boolean {
-    const descendants = this.treeControl.getDescendants(node);
-    const result = descendants.some(child => this.checklistSelection.isSelected(child));
-    return result && !this.descendantsAllSelected(node);
-  }
+	/** Whether all the descendants of the node are selected. */
+	descendantsAllSelected(node: TodoItemFlatNode): boolean {
+		const descendants = this.treeControl.getDescendants(node);
+		const descAllSelected = descendants.length > 0 && descendants.every(child => {
+		return this.checklistSelection.isSelected(child);
+		});
+		return descAllSelected;
+	}
+
+	/** Whether part of the descendants are selected */
+	descendantsPartiallySelected(node: TodoItemFlatNode): boolean {
+		const descendants = this.treeControl.getDescendants(node);
+		const result = descendants.some(child => this.checklistSelection.isSelected(child));
+		return result && !this.descendantsAllSelected(node);
+	}
+
+	private _buildQuerySettings(isSelected: boolean) {
+		const items = this.checklistSelection.selected;
+		console.log(items);
+	}
 
   /** Toggle the to-do item selection. Select/deselect all the descendants node */
-  todoItemSelectionToggle(node: TodoItemFlatNode): void {
-    this.checklistSelection.toggle(node);
-    const descendants = this.treeControl.getDescendants(node);
-    this.checklistSelection.isSelected(node)
-      ? this.checklistSelection.select(...descendants)
-      : this.checklistSelection.deselect(...descendants);
+	todoItemSelectionToggle(node: TodoItemFlatNode): void {
+		this.checklistSelection.toggle(node);
+		const descendants = this.treeControl.getDescendants(node);
+		const isSelected = this.checklistSelection.isSelected(node);
+		isSelected ? this.checklistSelection.select(...descendants)
+		: this.checklistSelection.deselect(...descendants);
 
-    // Force update for the parent
-    descendants.forEach(child => this.checklistSelection.isSelected(child));
-    console.log(descendants);
-    this.checkAllParentsSelection(node);
-  }
+	 // Force update for the parent
+	descendants.forEach(child => this.checklistSelection.isSelected(child));
+		this.checkAllParentsSelection(node);
+		this._buildQuerySettings(isSelected);
+}
 
   /** Toggle a leaf to-do item selection. Check all the parents to see if they changed */
-  todoLeafItemSelectionToggle(node: TodoItemFlatNode): void {
-    this.checklistSelection.toggle(node);
-    this.checkAllParentsSelection(node);
-  }
+	todoLeafItemSelectionToggle(node: TodoItemFlatNode): void {
+		this.checklistSelection.toggle(node);
+		this.checkAllParentsSelection(node);
+		this._buildQuerySettings(this.checklistSelection.isSelected(node));
+	}
 
 
-  /* Checks all the parents when a leaf node is selected/unselected */
-  checkAllParentsSelection(node: TodoItemFlatNode): void {
-    let query = {
-      key : node.value,
-      parents : []
-    }
-    let parent: TodoItemFlatNode | null = this.getParentNode(node);
-     while (parent) {
-      query.parents.unshift(parent.value);
-      this.checkRootNodeSelection(parent);
-      parent = this.getParentNode(parent);
-    }
-    console.log(query);
-    console.log(query.parents.join('.'));
-  }
+	/* Checks all the parents when a leaf node is selected/unselected */
+	checkAllParentsSelection(node: TodoItemFlatNode): void {
+		let query = {
+			key : node.value,
+			parents : []
+		}
+		let parent: TodoItemFlatNode | null = this.getParentNode(node);
+		while (parent) {
+		query.parents.unshift(parent.value);
+		this.checkRootNodeSelection(parent);
+		parent = this.getParentNode(parent);
+		}
+
+	}
 
   /** Check root node checked state and change it accordingly */
-  checkRootNodeSelection(node: TodoItemFlatNode): void {  
-    const nodeSelected = this.checklistSelection.isSelected(node);
-    const descendants = this.treeControl.getDescendants(node);
-    const descAllSelected = descendants.length > 0 && descendants.every(child => {
-      return this.checklistSelection.isSelected(child);
-    });
-    if (nodeSelected && !descAllSelected) {
-      this.checklistSelection.deselect(node);
-    } else if (!nodeSelected && descAllSelected) {
-      this.checklistSelection.select(node);
-    }
-  }
+	checkRootNodeSelection(node: TodoItemFlatNode): void {  
+		const nodeSelected = this.checklistSelection.isSelected(node);
+		const descendants = this.treeControl.getDescendants(node);
+		const descAllSelected = descendants.length > 0 && descendants.every(child => {
+		return this.checklistSelection.isSelected(child);
+		});
+		if (nodeSelected && !descAllSelected) {
+		this.checklistSelection.deselect(node);
+		} else if (!nodeSelected && descAllSelected) {
+		this.checklistSelection.select(node);
+		}
+	}
 
   /* Get the parent node of a node */
-  getParentNode(node: TodoItemFlatNode): TodoItemFlatNode | null {
-    const currentLevel = this.getLevel(node);
+	getParentNode(node: TodoItemFlatNode): TodoItemFlatNode | null {
+		const currentLevel = this.getLevel(node);
 
-    if (currentLevel < 1) {
-      return null;
-    }
+		if (currentLevel < 1) {
+		return null;
+		}
 
-    const startIndex = this.treeControl.dataNodes.indexOf(node) - 1;
+		const startIndex = this.treeControl.dataNodes.indexOf(node) - 1;
 
-    for (let i = startIndex; i >= 0; i--) {
-      const currentNode = this.treeControl.dataNodes[i];
+		for (let i = startIndex; i >= 0; i--) {
+		const currentNode = this.treeControl.dataNodes[i];
 
-      if (this.getLevel(currentNode) < currentLevel) {
-        return currentNode;
-      }
-    }
-    return null;
-  }
+		if (this.getLevel(currentNode) < currentLevel) {
+			return currentNode;
+		}
+		}
+		return null;
+	}
 }
